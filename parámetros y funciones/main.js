269f888cdb2024ce69dd2fb971ca524b21bdcc1c
anamontiaga <amontiaga@gmail.com>
2019-10-14 15:39:47 +0200

    add exercise

--- parámetros y funciones/main.js
+++ parámetros y funciones/main.js
@@ -1,7 +1,41 @@
 "use strict";
-const ten = word => {
-  for (let i = 0; i < 10; i++) {
-    console.log(word);
+// const writeThis = (word, number) => {
+//   for (let i = 0; i < number; i++) {
+//     console.log(word);
+//   }
+// };
+
+// const getRandomNumber = max => {
+//   return Math.ceil(Math.random() * max);
+// };
+
+// writeThis("carambola", getRandomNumber(10));
+
+const myWordList = [
+  {
+    text: "Pencil",
+    total: 6
+  },
+  {
+    text: "Thermo",
+    total: 2
+  },
+  {
+    text: "TV",
+    total: 8
+  },
+  {
+    text: "Phone",
+    total: 4
+  }
+];
+
+const writeMyArray = arr => {
+  for (let i = 0; i < arr.lenght; i++) {
+    for (let i = 0; i < arr[i].total; i++) {
+      console.log(arr[i].text);
+    }
   }
 };
-ten("carambola");
+
+writeMyArray(myWordList);
